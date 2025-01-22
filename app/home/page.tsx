d80f527dc48a460f1b26c7270ad5523533a9e777
Briankimp <bkimathi973@gmail.com>
2025-01-22 10:27:10 +0300

    Introduced the home

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -1,11 +1,19 @@
-import React from 'react'
+import Link from "next/link";
+import React from "react";
 
 const Home = () => {
   return (
-    <div>
-      Home
+    <div className="flex flex-col items-center justify-center ">
+      <h2 className="font-bold text-4xl ">Welcome to My Blog</h2>
+      <p className="text-xl text-center">
+        Here you'll find a variety of articles and tutorials on topics such as
+        web development, software engineering, and programming languages.{" "}
+      </p>
+      <Link href="/" className="hover:underline font-extrabold ">
+        View all Posts
+      </Link>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
